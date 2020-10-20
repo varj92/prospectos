201adb9e6e2d35f68528f01972fb88693401c575
unknown <julioe.valenciar@gmail.com>
2020-10-19 21:27:23 -0600

    se crean metodos para mostrar datos de prospectos en en listado.html

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for, Response
+from flask_pymongo import PyMongo
+from bson import json_util
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config['MONGO_URI']='mongodb://localhost/prospectos'
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/listado')
+@app.route('/listado', methods = ['GET'])
 def listado():
-    return render_template('listado.html')
+    prospectos = mongo.db.prospectos.find({})
+    return render_template('listado.html', prospectos = prospectos)
 
-@app.route('/evalucion')
+@app.route('/evaluacion', methods = ['GET'])
 def evaluacion():
-    return render_template('evaluacion.html')
+    prospectos = mongo.db.prospectos.find({})
+    return render_template('evaluacion.html', prospectos = prospectos)
 
 if __name__ == '__main__':
     app.run(debug=True)
